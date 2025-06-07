65f34bf8df6d3bf4fb0b98b84c1fb5d96231519a
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-06-07 16:54:44 +0530

    Add CORS support and set up server with Express

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use('/api/users', require('./routes/userRoutes'));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
