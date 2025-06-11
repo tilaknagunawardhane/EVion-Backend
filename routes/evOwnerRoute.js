7d9983d1e3654c6e66f86e8948d4290dfaf53284
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-06-11 22:00:23 +0530

    Add OTP verification functionality and update routes for OTP handling

--- routes/evOwnerRoute.js
+++ routes/evOwnerRoute.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { registerEvOwner, loginEvOwner } = require('../controllers/evOwnerController');
+const { registerEvOwner, loginEvOwner, sendOTP, verifyOTP } = require('../controllers/evOwnerController');
 
 // Route to register a new EV owner
 router.post('/register', registerEvOwner);
 // Route to login an existing EV owner
 router.post('/login', loginEvOwner);
+
+router.post('/send-otp', sendOTP);
+router.post('/verify-otp', verifyOTP);
+
 
 module.exports = router;
