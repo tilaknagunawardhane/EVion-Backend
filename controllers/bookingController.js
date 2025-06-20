e423e10dafaf0a4f7a2aac8454657c5df3c37651
Mavishan01 <janithmavishan01@gmail.com>
2025-06-20 18:49:55 +0530

    booking date updated

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,8 +1,11 @@
 const Booking = require('../models/bookingModel');
 const asyncHandler = require('express-async-handler');
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
 const slot_size = process.env.SLOT_SIZE;
 
+// Extend dayjs with UTC plugin
+dayjs.extend(utc);
 
 const addBooking = asyncHandler(async (req, res) => {
     let {booking_date_time, no_of_slots, charger_id, plug_type} = req.body;
@@ -12,18 +15,31 @@ const addBooking = asyncHandler(async (req, res) => {
         res.status(400);      
         throw new Error('Please fill in all booking fields');
     }
+    
+    booking_date_time = dayjs(booking_date_time).utc(); //with utc(), string/date -> date in Z   
+    console.log(booking_date_time.format());
 
-    // console.log(booking_date_time);
-    booking_date_time = dayjs(booking_date_time);
     if (booking_date_time.isValid()){
-        console.log(booking_date_time.format());
+
+        booking_date = booking_date_time.startOf('day'); //date & time UTC -> date with midnight UTC
+        start_time = booking_date_time.format('HH:mm:ss'); //in UTC as a string
+        end_time = booking_date_time.add(slot_size*no_of_slots, 'minute').format('HH:mm:ss'); //in UTC as a string
+        
+        // console.log(booking_date.format());
+        // console.log(start_time);
+        // console.log(end_time);
     }
     else{
         console.log('Invalid Date!!');
     }
 
+    const existingBooking = await Booking.findOne({ booking_date_time: {$lt: booking_date_time}})
+
     const booking = await Booking.create({
         booking_date_time,
+        booking_date,
+        start_time,
+        end_time,
         no_of_slots,
         charger_id,
         plug_type
@@ -31,7 +47,14 @@ const addBooking = asyncHandler(async (req, res) => {
 
     if (booking){
         res.status(201).json({ 
-            //
+            _id: booking._id,
+            booking_date_time: booking.booking_date_time,
+            booking_date: booking.booking_date,
+            start_time: booking.start_time,
+            end_time: booking.end_time,
+            no_of_slots: booking.no_of_slots,
+            charger_id: booking.charger_id,
+            plug_type: booking.plug_type
         });
     }else {
         res.status(400);
