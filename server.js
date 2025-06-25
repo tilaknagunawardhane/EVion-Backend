716fa0a064cc0c2a575962982cd3648e9c32c3e2
Mavishan01 <janithmavishan01@gmail.com>
2025-06-26 01:22:29 +0530

    getting make and model names in a common file

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.json());
 // app.use('/api/users', require('./routes/userRoutes'));
 const evOwnerRoutes = require('./routes/evOwnerRoute');
 const bookingRoutes = require('./routes/bookingRoute');
-const vehicleModelRoutes = require('./routes/vehicleModelRoute');
+const commonRoutes = require('./routes/commonRoute');
 
 app.use('/api/evowners', evOwnerRoutes);
 app.use('/api/bookings', bookingRoutes);
-app.use('/api/vehiclemodels', vehicleModelRoutes);
+app.use('/api/common', commonRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
