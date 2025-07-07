7f90bb25deb1e6db3d57cd995303406908b0d6da
Mavishan01 <janithmavishan01@gmail.com>
2025-07-07 18:03:53 +0530

    adding new fields

--- models/connectorModel.js
+++ models/connectorModel.js
@@ -1,9 +1,18 @@
+const { urlencoded } = require('express');
 const mongoose = require('mongoose');
 
 const connectorSchema = new mongoose.Schema({
     type_name: {
         type: String,
         required: true
+    },
+    current_type: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: URL,
+        required: false
     }
 },
     {timestamps: true});
