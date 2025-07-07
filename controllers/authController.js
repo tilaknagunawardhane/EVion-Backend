3fc834c2c791efe52f3b70232c38e91489ae6aa3
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-07-07 15:45:34 +0530

    cookieParser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { generateToken } = require('../config/jwt');
 const Admin = require('../models/adminModel');
 const EvOwner = require('../models/evOwnerModel');
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
