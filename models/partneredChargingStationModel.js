23341c011e6323d6cf624737236f4b5f13a11ebc
Mavishan01 <janithmavishan01@gmail.com>
2025-07-12 11:31:29 +0530

    populating the vehicle make into in vehiclemodel on save and update

--- models/partneredChargingStationModel.js
+++ models/partneredChargingStationModel.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const partneredChargingStationSchema = new mongoose.Schema({
     station_owner_id: {
         type: mongoose.Schema.Types.ObjectId,
-        // ref: 
+        ref: 'stationowner',
         required: true
+    },
+    station_owner_info: {
+        name: String
     },
     station_name: {
         type: String,
@@ -16,6 +19,23 @@ const partneredChargingStationSchema = new mongoose.Schema({
     }
 },
     {timestamps: true});
+
+//pre save hook
+partneredChargingStationSchema.pre('save', async function (next){
+    if (this.isModified('station_owner_id') || this.isNew){
+        try{
+            const station_owner_doc = await mongoose.model('stationowner').findById(this.station_owner_id);
+            if(station_owner_doc){
+                this.station_owner_info = {
+                    name: station_owner_doc.name
+                };
+            }
+        } catch(err){
+            return next(err);
+        }
+    }
+    next();
+});
 
 const PartneredChargingStation = mongoose.model('partneredChargingStation', partneredChargingStationSchema);
 module.exports = PartneredChargingStation;
--- models/vehiclemodelModel.js
+++ models/vehiclemodelModel.js
@@ -7,11 +7,53 @@ const vehiclemodelSchema = new mongoose.Schema({
     },
     make: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'makes',
+        ref: 'vehiclemake',
         required: true
+    },
+    make_info: {
+        make: String
     }
 },
     {timestamps: true});
+
+// Pre save hook
+vehiclemodelSchema.pre('save', async function (next) {
+    // populate if field was modified or on first save
+    if (this.isModified('make') || this.isNew) {
+        try {
+            const makeDoc = await mongoose.model('vehiclemake').findById(this.make);
+            if (makeDoc) {
+                this.make_info = {
+                    make: makeDoc.make
+                };
+            }
+        } catch (err) {
+            return next(err);
+        }
+    }
+    next();
+});
+
+// Pre findOneAndUpdate hook for update queries
+vehiclemodelSchema.pre('findOneAndUpdate', async function (next) {
+    const update = this.getUpdate();
+
+    if (update.make) {
+        try {
+            const makeDoc = await mongoose.model('vehiclemake').findById(update.make);
+            if (makeDoc) {
+                update.make_info = {
+                    make: makeDoc.make
+                };
+                this.setUpdate(update);
+            }
+        } catch (err) {
+            return next(err);
+        }
+    }
+
+    next();
+});
 
 const VehicleModel = mongoose.model('vehiclemodel', vehiclemodelSchema);
 module.exports = VehicleModel;
