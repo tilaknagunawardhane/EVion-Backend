ca3542caa27f18ecaccdda1f5f6757f46a30eef8
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-07-15 09:44:55 +0530

    deactivate vehicle

--- routes/vehicleRoute.js
+++ routes/vehicleRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getDropdownData, getConnectorTypes, uploadVehicleImage, addVehicle, fetchVehicles, getVehicleByID} = require('../controllers/vehicleController');
+const {getDropdownData, getConnectorTypes, uploadVehicleImage, addVehicle, fetchVehicles, getVehicleByID, deactivateVehicle} = require('../controllers/vehicleController');
 
 router.get('/dropdowndata', getDropdownData);
 router.get('/connectors', getConnectorTypes);
 router.post('/addVehicle', uploadVehicleImage, addVehicle);
 router.post('/fetchVehicles', fetchVehicles );
 router.post('/getVehicleByID', getVehicleByID);
+router.post('/deactivateVehicle', deactivateVehicle);
 
 module.exports = router;
