579b5a43cae635971e42d2eb235b5cc7439f7b62
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-07-21 05:04:19 +0530

    Admin Login connected with backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -97,6 +97,7 @@ const registerUser = async (Model, req, res) => {
 
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
+        console.log('Hashed password: ', hashedPassword);
 
         user = await Model.create({
             name,
