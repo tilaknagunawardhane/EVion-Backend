25c4f0acabbb6dbe67d08d6602c27730c12d6518
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-07-20 19:55:58 +0530

    update station request

--- routes/partneredChargingStationRoute.js
+++ routes/partneredChargingStationRoute.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { checkStationsExist, createStation, getRequestedStations, deleteStation } = require('../controllers/partneredChargingStationController');
+const { checkStationsExist, createStation, getRequestedStations, deleteStation, updateStation, getStationForEdit } = require('../controllers/partneredChargingStationController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 // router.get('/check-stations', authMiddleware(['station-owner'], ['station-owner']), checkStationsExist);
 router.post('/check-stations', checkStationsExist);
 router.post('/create-station', createStation);
 router.post('/get-request-stations', getRequestedStations);
-router.delete('/delete-station/:id', deleteStation)
+router.delete('/delete-station/:id', deleteStation);
+router.post('/edit-station/:id', getStationForEdit);
+router.put('/update-station/:id', updateStation);
 
 module.exports = router;
