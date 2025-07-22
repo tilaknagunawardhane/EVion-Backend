3047a283a7ec02624fdb8abe51ed7654a6797ddf
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-07-22 08:04:41 +0530

    admin view request details

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {
-    getAdminRequests
+    getAdminRequests,
+    getRequestDetails
 } = require('../controllers/adminController');
 
 router.get('/get-requests', getAdminRequests);
+router.get('/request-details/:id', getRequestDetails);
 
 module.exports = router;
