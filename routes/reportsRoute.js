f793b9c08bfe586b9c098a635018a0f17a9aefcf
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-08-21 13:58:31 +0530

    Add booking-related models and implement booking details retrieval

--- routes/reportsRoute.js
+++ routes/reportsRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { submitStationReport,
-    submitChargerReport
+    submitChargerReport,
+    getBookingDetails
  } = require('../controllers/reportsController');
 
 router.post('/submit-report', submitStationReport);
 router.post('/submit-charger-report', submitChargerReport);
+router.get('/booking-details/:bookingId', getBookingDetails);
 
 module.exports = router;
