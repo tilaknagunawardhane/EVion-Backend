85d32e2a1f8a97819ea8000a2342fbce88d0f30b
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-08-23 23:24:42 +0530

    report handling completed

--- routes/reportsRoute.js
+++ routes/reportsRoute.js
@@ -7,7 +7,8 @@ const { submitStationReport,
     submitBookingReport,
     getAllReports,
     getReportDetails,
-    saveReportAction
+    saveReportAction,
+    updateRefund
  } = require('../controllers/reportsController');
 
 router.post('/submit-report', submitStationReport);
@@ -17,5 +18,6 @@ router.post('/submit-booking-report', submitBookingReport);
 router.get('/all-reports', getAllReports);
 router.get('/report-details/:type/:id', getReportDetails);
 router.put('/save-report-action/:type/:id/action', saveReportAction);
+router.put('/refund-reports/:type/:id/refund', updateRefund);
 
 module.exports = router;
