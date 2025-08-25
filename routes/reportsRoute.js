93f7fca7450c20cd7853ab73a2dc062f7bd23825
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-08-25 22:16:05 +0530

    Fault report section fetch data

--- routes/reportsRoute.js
+++ routes/reportsRoute.js
@@ -8,7 +8,9 @@ const { submitStationReport,
     getAllReports,
     getReportDetails,
     saveReportAction,
-    updateRefund
+    updateRefund,
+    getEvOwnerReports,
+    getEvOwnerReportDetails
  } = require('../controllers/reportsController');
 
 router.post('/submit-report', submitStationReport);
@@ -19,5 +21,8 @@ router.get('/all-reports', getAllReports);
 router.get('/report-details/:type/:id', getReportDetails);
 router.put('/save-report-action/:type/:id/action', saveReportAction);
 router.put('/refund-reports/:type/:id/refund', updateRefund);
+
+router.get('/get-evowner-reports/:userId', getEvOwnerReports);
+router.get('/get-evowner-report-details/:userId/:type/:reportId', getEvOwnerReportDetails);
 
 module.exports = router;
