a6efe532a2b99cfdad1ac7f2da3068a42dac70d9
Sadewni <saduniwehallage@gmail.com>
2025-08-28 07:49:32 +0530

    discussion routes added

--- routes/discussionRoutes.js
+++ routes/discussionRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { createDiscussion, getAllDiscussions } = require('../controllers/discussionController');
 
+const { 
+  createDiscussion, 
+  getAllDiscussions, 
+  getDiscussionById, 
+  addComment 
+} = require('../controllers/discussionController');
+
+// Routes
 router.post('/create-discussion', createDiscussion);
 router.get('/get-discussions', getAllDiscussions);
+router.get('/discussions/:id', getDiscussionById);
+router.post('/discussions/:id/comments', addComment);
 
 module.exports = router;
